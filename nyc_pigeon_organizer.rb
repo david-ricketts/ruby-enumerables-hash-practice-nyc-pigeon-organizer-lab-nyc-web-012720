f09ea60911972a49a92c3157af5a672ada7273cd
David Ricketts <david.ricketts119@gmail.com>
2020-01-26 15:42:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |title, value|
+    value.each do |information_colected, names|
+      names.each do |name|
+        if pigeons[name] == nil
+          pigeons[name] = {}
+        end
+        if pigeons[name][title] == nil
+          pigeons[name][title] = {}
+        end
+        pigeons[name][title][title].push(information_colected.to_s)
+      end
+    end
+  end
+  pigeons
 end
